7020c520228aa7b411684414983f80cb3e16eed3
rosaann <147429240@qq.com>
2019-04-01 17:05:52 +0800

    d

--- train.py
+++ train.py
@@ -56,8 +56,8 @@ def train_classifier_single_epoch(config, model, dataloader, criterion, optimize
             images = images.cuda()
             labels = labels.cuda()
         logits, aux_logits, probabilities = inference(model, images)
-        print('logits ', logits.shape)
-        print('labels ', labels.shape)
+      #  print('logits ', logits.shape)
+      #  print('labels ', labels.shape)
         loss = criterion(logits, labels.float())
         if aux_logits is not None:
             aux_loss = criterion(aux_logits, labels.float())
