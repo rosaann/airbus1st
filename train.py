1b8260bd5e13714894b9c0f2fce34c5820f482f5
rosaann <147429240@qq.com>
2019-04-22 16:13:20 +0800

    d

--- train.py
+++ train.py
@@ -196,7 +196,8 @@ def train_segmenter_single_epoch(config, model, dataloader, criterion, optimizer
             gt = gt.cuda()
         
         binary_masks = model(images)
-
+        print('binary_masks ', binary_masks )
+        print('gt ', gt)
         loss = criterion(binary_masks, gt)
 
             # measure accuracy and record loss
