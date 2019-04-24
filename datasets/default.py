5e54dae0ad3f502ddb891ab65a1d607b3060ef07
rosaann <147429240@qq.com>
2019-04-24 09:45:52 +0800

    d

--- datasets/default.py
+++ datasets/default.py
@@ -64,17 +64,17 @@ class DefaultSegmenterDataset(Dataset):
         
         gt_path = example['gt']
       #  print('gt_path ', gt_path)
-        gt_img = cv2.imread(gt_path)
-      #  print('gt_img ', gt_img.shape)
+        gt_img = cv2.imread(gt_path, 0)
+        print('gt_img ', gt_img.shape)
         
         transform_gt = transforms.Compose([
              #   transforms.ToPILImage(),
-                transforms.ToTensor(), # range [0, 255] -> [0.0,1.0]
+                transforms.ToTensor(basic=True), # range [0, 255] -> [0.0,1.0]
                 ])
       #  gt_img = gt_img * 255
         gt_img = transform_gt(gt_img)
       #  print('gt_img2 ', gt_img)
-        gt_img = torch.sum(gt_img, dim = 0).type(torch.LongTensor)
+      #  gt_img = torch.sum(gt_img, dim = 0).type(torch.LongTensor)
       #  gt_img = gt_img.type(torch.LongTensor)
         print('gt_img3 ', gt_img)
 
